08d5505ae6ca66f999ce81e08da3218dd2186d41
Billy <billybofh@gmail.com>
2020-06-20 16:09:24 +0100

    Tidy some html

--- resources/views/livewire/database-restorer.blade.php
+++ resources/views/livewire/database-restorer.blade.php
@@ -3,13 +3,13 @@
         <form action="">
             <input class="sr-only" @focus="focused = true" @blur="focused = false" id="restore-file" accept=".zip" type="file" wire:model="restoreFile">
             @if ($restoreFile)
-                <label id="tweedle" :class=" {'outline-none underline': focused }" class="default-link cursor-pointer" for="restore-file" wire:click.prevent="restore">Restore {{ $restoreFile->getClientOriginalName() }}</label>
+                <label id="tweedle" :class=" {'outline-none underline': focused }" class="default-link cursor-pointer" for="restore-file" wire:click.prevent="restore">Restore @error('restoreFile') @else $restoreFile->getClientOriginalName() @enderror</label>
             @else
                 <label id="dum" :class=" {'outline-none underline': focused }" class="default-link cursor-pointer" for="restore-file">Restore</label>
             @endif
+            @error('restoreFile')
+                <span class="text-red-500">Invalid!</span>
+            @enderror
         </form>
-        @error('restoreFile')
-            <span class="text-red-500">Invalid!</span>
-        @enderror
     </span>
 </div>
